30fa9ecebd52258dafc38ceb0f013bfce8004740
gd <gd@gd-K43SJ>
2013-08-05 11:15:06 +0800

    a

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from read_pcap import read_pcap
 from ip_to_city import ip_to_city
 from static import static
 from fpdf import FPDF
+import time
 
 def main():
     import ConfigParser
@@ -16,17 +17,28 @@ def main():
     fpcap = file(pcapfile,"rb")
     http_datas = read_pcap(fpcap)
     
-    hosts = static(http_datas)
-    print hosts
-    
+    top_hosts,hosts = static(http_datas)
     #write result in pdf
     pdf=FPDF()
     pdf.add_page()
-    pdf.set_font('Arial','B',16)
-    i = 0
-    for host in hosts:
-        pdf.cell(70,30+i,host)
-        i = i + 30
+    pdf.set_font('Arial','B',24)
+    pdf.cell(60,10,'PCAP Analysis results','C')
+    pdf.ln(20)
+    for i in range(1,2):
+        pdf.set_font('Arial','B',16)
+        pdf.cell(70,20,"top%d"%i)
+        pdf.ln(10)
+        pdf.set_font('Arial','B',12)
+        pdf.cell(70,20,"host:"+str(top_hosts[i-1]))
+        pdf.ln(10)
+        pdf.cell(70,20,"num:"+str(hosts[top_hosts[i-1]]['num']))
+        pdf.ln(10)
+        pdf.cell(70,20,"visitors:")
+        pdf.set_font('Arial','B',10)
+        for visitor in hosts[top_hosts[i-1]]['visitors']:
+            pdf.ln(7)
+            print visitor
+            pdf.cell(200,20,"     time:"+time.strftime('%Y-%m-%d',time.localtime(float(visitor[0])))+"     ip"+str(visitor[1])+"     city:"+str(visitor[2:3]))
     pdf.output('results.pdf','F')
     
     #for http_data in http_datas:
@@ -34,4 +46,4 @@ def main():
       #  print ip_to_city(http_data['srcip'])
 
 if __name__ == "__main__":    
-    main()    
+    main()  
--- static.py
+++ static.py
@@ -1,19 +1,22 @@
 # coding = utf-8
+from ip_to_city import ip_to_city
 
 def static(records):
     '''static the same host in the packet and cacl the top 10 welcome hosts
+       return a top_host list and a dict of host{"host":{'num':1,'visitor':[...]}[,]}
     '''
     hosts = {}
     for record in records:
         if record['host'] in hosts.keys():
             hosts[record['host']]['num'] += 1
-            hosts[record['host']]['visitors'].append((record['time'],record['srcip']))
+            addr = ip_to_city(record['srcip'])
+            hosts[record['host']]['visitors'].append((record['time'],record['srcip'],addr))
         else:
             hosts[record['host']] = {'num':1,'visitors':[(record['time'],record['srcip'])]}
             
     #cacl the top 10 welcome hosts
-    top_hosts = sorted(hosts,reverse=True)     
-    return top_hosts            
+    top_hosts = sorted(hosts,reverse=True)      
+    return top_hosts,hosts            
 
 
 if __name__=="__main__":
